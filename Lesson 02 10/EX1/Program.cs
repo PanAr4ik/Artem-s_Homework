adb39b09fa70119f8286526e4c360fa0d804aa99
PanAr4ik <tistol.artem@gmail.com>
2024-10-05 09:51:21 +0300

    Add files via upload

---
+++ Lesson 02 10/EX1/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace EX1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Income salary = new Income(IncomeType.Salary, 10000);
+            Income businessIncome = new Income(IncomeType.Business, 20000);
+
+            float salaryTax = TaxConfig.CalculateTax(salary);
+            float businessTax = TaxConfig.CalculateTax(businessIncome);
+
+            Console.WriteLine($"Налог с зарплаты: {salaryTax}");
+            Console.WriteLine($"Налог с бизнеса: {businessTax}");
+            Console.WriteLine($"Общая сумма налогов: {TaxConfig.GetTotalTaxesCollected()}");
+        }
+    }
+
+    public enum IncomeType
+    {
+        Salary,
+        Business,
+        Investments
+    }
+
+    public class Income
+    {
+        public IncomeType Type { get; }
+        public float Amount { get; }
+
+        public Income(IncomeType type, float amount)
+        {
+            Type = type;
+            Amount = amount;
+        }
+    }
+
+    public static class TaxConfig
+    {
+        private static float totalTaxesCollected = 0;
+
+        public static float SalaryTaxRate { get; private set; } = 0.13f; 
+        public static float BusinessTaxRate { get; private set; } = 0.25f;
+        public static float InvestmentTaxRate { get; private set; } = 0.15f;
+
+        public static float CalculateTax(Income income)
+        {
+            float taxRate = GetTaxRate(income.Type);
+            float taxAmount = income.Amount * taxRate;
+            totalTaxesCollected += taxAmount;
+            return taxAmount;
+        }
+
+        private static float GetTaxRate(IncomeType type)
+        {
+            switch (type)
+            {
+                case IncomeType.Salary:
+                    return SalaryTaxRate;
+                case IncomeType.Business:
+                    return BusinessTaxRate;
+                case IncomeType.Investments:
+                    return InvestmentTaxRate;
+                default:
+                    throw new ArgumentException("Unknown income type");
+            }
+        }
+
+        public static float GetTotalTaxesCollected()
+        {
+            return totalTaxesCollected;
+        }
+    }
+}
---
+++ Lesson 02 10/EX2/Program.cs
@@ -0,0 +1,102 @@
+﻿namespace EX2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Война и мир", "Л. Толстой", BookFormat.Hardcover, BookGenre.Fiction);
+            Book book2 = new Book("1984", "Дж. Оруел", BookFormat.Paperback, BookGenre.Fiction);
+            Book book3 = new Book("S-T-I-K-S", "А. Кам`янистий", BookFormat.Hardcover, BookGenre.Fiction);
+
+            Library.AddBook(book1);
+
+            if (Library.TakeBook(book1))
+            {
+                Console.WriteLine("Книга выдана.");
+            }
+
+            Console.WriteLine($"Количество взятых книг: {Library.TotalBooksTaken}");
+        }               
+    }
+    public enum BookFormat
+    {
+        Hardcover,
+        Paperback,
+        Ebook,
+        Audiobook
+    }
+
+    public enum BookGenre
+    {
+        Fiction,
+        NonFiction,
+        Mystery,
+        ScienceFiction
+    }
+
+    public class Book
+    {
+        public string Title { get; }
+        public string Author { get; }
+        public BookFormat Format { get; }
+        public BookGenre Genre { get; }
+
+        public Book(string title, string author, BookFormat format, BookGenre genre)
+        {
+            Title = title;
+            Author = author;
+            Format = format;
+            Genre = genre;
+        }
+    }
+
+    public static class LibraryConfig
+    {
+        public static readonly int MaxBooks = 1000; 
+    }
+
+    public static class Library
+    {
+        private static List<Book> _books = new List<Book>();
+        private static int _booksTaken = 0;
+
+        public static void AddBook(Book book)
+        {
+            if (_books.Count < LibraryConfig.MaxBooks)
+            {
+                _books.Add(book);
+            }
+            else
+            {
+                Console.WriteLine("Превышено максимальное количество книг в каталоге.");
+            }
+        }
+
+        public static bool TakeBook(Book book)
+        {
+            if (_books.Remove(book))
+            {
+                _booksTaken++;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Книги нет в наличии.");
+                return false;
+            }
+        }
+
+        public static void ReturnBook(Book book)
+        {
+            _books.Add(book);
+            _booksTaken--;
+        }
+
+        public static bool IsBookAvailable(Book book)
+        {
+            return _books.Contains(book);
+        }
+
+        public static int TotalBooksTaken => _booksTaken;
+    }
+}
